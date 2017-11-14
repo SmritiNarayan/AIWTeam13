99668546655b7007696d5e7086c13be9eed2a9c9
rishabhberi <rishabhberi2606@gmail.com>
2017-11-14 21:22:57 +0530

    Bug fixes

--- part2/rishabh/start.js
+++ part2/rishabh/start.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', init);
+
 function init(){
+	localStorage.help = 0;
 	x=document.getElementById("startButton");
 	x.onclick=storeInDb;
 }
+
 function storeInDb(){
 	//store in Db that he skipped or a negative time
         chrome.tabs.update({'url':'first.html'});
