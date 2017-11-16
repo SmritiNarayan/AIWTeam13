152f2952b4078f945f79ab8da37c1958d29c10b6
rishabhberi <rishabhberi2606@gmail.com>
2017-11-16 20:16:26 +0530

    Bug fixes

--- part2/rishabh/fonts.js
+++ part2/rishabh/fonts.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', init);
 fontChange=false;
 function init(){
 	localStorage.startTime = (new Date()).getTime();
+	p = document.getElementById("p");
 	var button = document.getElementById("button");
 	var here = document.getElementById("here");
 	var hidden = document.getElementById("hidden");
 	hidden.style.display = 'none';
-	button.onclick = submit;
+	button.onclick = notComplete;
 	here.onclick = reveal;
 	fontChange=false;
 	chrome.fontSettings.onDefaultFontSizeChanged.addListener(callback);
@@ -14,8 +15,17 @@ function init(){
 
 function callback(details){
 	fontSize = details.pixelSize;
-	fontChange = true;
-	button.value = fontSize;
+	chrome.fontSettings.onDefaultFontSizeChanged.removeListener(callback);
+	//update in db
+	localStorage.endTime = (new Date()).getTime();
+	localStorage.third = localStorage.endTime - localStorage.startTime;
+	chrome.tabs.update({'url':'fourth.html'});
+	// button.value = fontSize;
+}
+
+function notComplete(){
+	localStorage.third=-1;
+	chrome.tabs.update({'url':'fourth.html'});
 }
 
 function reveal(){
@@ -23,15 +33,4 @@ function reveal(){
 	// user may not be an expert!!
 	// update in db
 	localStorage.help = parseInt(localStorage.help) + 1;
-}
-
-function submit(){
-	chrome.fontSettings.onDefaultFontSizeChanged.removeListener(callback);
-	//update in db
-	localStorage.endTime = (new Date()).getTime();
-	if(fontChange == true)
-		localStorage.third = localStorage.endTime - localStorage.startTime;
-	else
-		localStorage.third=-1;
-	chrome.tabs.update({'url':'fourth.html'});
 }
