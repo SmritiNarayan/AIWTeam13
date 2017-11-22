f4c33b06b2b6ee92440170066209e52965bf4de8
slothie26 <vidhuaditi@gmail.com>
2017-11-23 00:13:40 +0530

    added customizations for beginenr and intermediate

--- part2/rishabh/beginner.js
+++ part2/rishabh/beginner.js
@@ -1,4 +1,24 @@
-a = document.getElementById("a");
+a = document.getElementById("settings");
 a.onclick = function(){
 	chrome.tabs.update({'url':'chrome://settings'});
+};
+b = document.getElementById("bookmarks");
+b.onclick = function(){
+	chrome.tabs.update({'url':'chrome://bookmarks'});
+};
+c = document.getElementById("history");
+c.onclick = function(){
+	chrome.tabs.update({'url':'chrome://history'});
+};
+d = document.getElementById("downloads");
+d.onclick = function(){
+	chrome.tabs.update({'url':'chrome://downloads'});
+};
+e = document.getElementById("newtab");
+e.onclick = function(){
+	chrome.tabs.update({'url':'chrome://newtab'});
+};
+f = document.getElementById("version");
+f.onclick = function(){
+	chrome.tabs.update({'url':'chrome://version'});
 };
--- part2/rishabh/navMovement.js
+++ part2/rishabh/navMovement.js
@@ -7,6 +7,30 @@ y=document.getElementById("xxspan");
 y.onclick = openNav;
 z= document.getElementById("sbsbutton");
 z.onmouseover = randomweirdfn;
+a = document.getElementById("settings");
+a.onclick = function(){
+	chrome.tabs.update({'url':'chrome://settings'});
+};
+b = document.getElementById("bookmarks");
+b.onclick = function(){
+	chrome.tabs.update({'url':'chrome://bookmarks'});
+};
+c = document.getElementById("history");
+c.onclick = function(){
+	chrome.tabs.update({'url':'chrome://history'});
+};
+d = document.getElementById("downloads");
+d.onclick = function(){
+	chrome.tabs.update({'url':'chrome://downloads'});
+};
+e = document.getElementById("newtab");
+e.onclick = function(){
+	chrome.tabs.update({'url':'chrome://newtab'});
+};
+f = document.getElementById("version");
+f.onclick = function(){
+	chrome.tabs.update({'url':'chrome://version'});
+};
 }
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
